608f3930a4bf346663e0f111ddb45207741a2097
EmmKar <emma.karlsmose@hotmail.com>
2021-03-24 20:53:59 +0100

    Rough account management skeleton. The only thing that has thus far cooperated fully

---
+++ src/main/java/com/mindfultrader/webapp/controllers/EditAccountController.java
@@ -0,0 +1,60 @@
+package com.mindfultrader.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * Controller for user account pages. 
+ * 
+ * User must be able to change password
+ * User must be able to change their email
+ * User must be able to delete their account
+ * 
+ */
+
+@Controller
+public class EditAccountController {
+	
+	
+	// Main page for account edits
+	@RequestMapping("/account")
+	public String editAccount()
+	{
+		return "accountManagement/account";
+	}
+	
+	
+	// Warning message for account deletion
+	@RequestMapping("/account/delete")
+	public String confirmAccountDeletion()
+	{
+		return "accountManagement/delete_account";
+	}
+	
+	
+	// Delete account and all details
+	@RequestMapping("/account/delete/submit")
+	public String deleteAccount()	
+	{
+		//CODE HERE
+		
+		return "accountManagement/bye";
+	}
+	
+	// Edit email
+	@RequestMapping("/account/editEmail")
+	public String editEmail()
+	{
+		//CODE HERE
+		return "accountManagement/change_email";
+	}
+	
+	// Edit password
+	@RequestMapping("/account/changePassword")
+	public String editPassword()
+	{
+		//CODE HERE
+		return "accountManagement/change_password";
+	}
+
+}
