6fba0a8434d1ceb26bfd0795e51508bd0cb3c399
Mindful Trader <mindfultraderproject@gmail.com>
2021-04-30 13:40:04 +0100

    make it better

--- src/main/java/com/mindfultrader/webapp/controllers/EditAccountController.java
+++ src/main/java/com/mindfultrader/webapp/controllers/EditAccountController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
